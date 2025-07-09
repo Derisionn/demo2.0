4652d3311f378ccff425ae111775163cac54061e
Harsh Vardhan <harshvardhan8630466531@gmail.com>
2025-07-09 10:53:48 +0530

    Add Demo <3

---
+++ index.py
@@ -0,0 +1,8 @@
+import streamlit as st 
+
+st.header("Demo <3 ")
+# name =st.selectbox("Select Your Name",["Harsh Vardhan","vabhi","Tanishq","Vineet"])
+# if name !="" :
+#     st.write(f"Hi {name} !!")
+# else:
+#     pass
