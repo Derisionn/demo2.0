86de50f5134dde94d7770b645d065757b2787f1d
Harsh Vardhan <harshvardhan8630466531@gmail.com>
2025-07-09 11:07:36 +0530

    Add in master branch

--- index.py
+++ index.py
@@ -1,8 +1,13 @@
 import streamlit as st 
 
 st.header("Demo <3 ")
-# name =st.selectbox("Select Your Name",["Harsh Vardhan","vabhi","Tanishq","Vineet"])
-# if name !="" :
-#     st.write(f"Hi {name} !!")
-# else:
-#     pass
+
+
+
+name =st.selectbox("Select Your Name",["Harsh Vardhan","vabhi","Tanishq","Vineet"])
+
+
+if name !="" :
+    st.write(f"Hi {name} !!")
+else:
+    pass
